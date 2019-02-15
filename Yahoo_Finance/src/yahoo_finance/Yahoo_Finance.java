00b68356d1959fa7455ac5e5185dd25ba7b8b048
mbg-nimda <mbg.nimda@gmail.com>
2019-02-15 13:38:41 +0100

    Initial commit from netbeans 10 This is a subproject from the ShowStock project meant for downloading .csv files from the Yahoo Finance site

---
+++ Yahoo_Finance/src/yahoo_finance/Yahoo_Finance.java
@@ -0,0 +1,91 @@
+/**
+ *
+ */
+package yahoo_finance;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * @author nimda
+ *
+ */
+public class Yahoo_Finance {
+
+    private static Connection c;
+    private static Statement stm;
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:E:/Bolero/stocks.db");
+            stm = c.createStatement();
+            String cmd = "select symb from tickers order by symb";
+            ResultSet rS = stm.executeQuery(cmd);
+            List<String> stocks = new ArrayList();
+            while (rS.next()) {
+                stocks.add(rS.getString("symb"));
+            }
+
+            /*
+            System.setProperty("webdriver.gecko.driver","E:/Selenium/geckodriver.exe");
+            WebDriver browser = new FirefoxDriver();
+             */
+            System.setProperty("webdriver.chrome.driver", "E:/Selenium/chromedriver.exe");
+            WebDriver browser = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(browser, 60);
+//		browser.get("https://finance.yahoo.com/portfolio/p_0/view/v1"); 
+//		browser.get("https://login.yahoo.com/?.src=finance&.intl=us&.done=https%3A%2F%2Ffinance.yahoo.com%2Fportfolios&add=1");
+
+            for (String tick : stocks) {
+                String url = String.format("https://finance.yahoo.com/quote/%s/history?p=%s", tick, tick);
+                browser.get(url);
+                if (browser.getTitle().contains("Oath")) {
+                    WebElement oath = browser.findElement(By.name("agree"));
+                    oath.click();
+                }
+                /*
+    * Uncomment if more than 22 days ago
+    
+    String xpath =
+    "//*[@id=\"Col1-1-HistoricalDataTable-Proxy\"]/section/div[1]/div[1]/button";
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+    WebElement apply = browser.findElement(By.xpath(xpath));
+    apply.click();
+    try {
+    Thread.sleep(4000);
+    } catch (InterruptedException e) {
+    e.printStackTrace();
+    }
+    *
+                 */
+                String slcr = "#Col1-1-HistoricalDataTable-Proxy > section > div.Mt\\28 15px\\29.drop-down-selector.historical > div.C\\28 \\24 c-fuji-grey-j\\29.Mt\\28 20px\\29.Mb\\28 15px\\29 > span.Fl\\28 end\\29.Pos\\28 r\\29.T\\28 -6px\\29 > a";
+                String xpath = "//*[@id=\"Col1-1-HistoricalDataTable-Proxy\"]/section/div[1]/div[2]/span[2]/a";
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(slcr)));
+                WebElement download = browser.findElement(By.cssSelector(slcr));
+                download.click();
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            browser.close();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Yahoo_Finance.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
